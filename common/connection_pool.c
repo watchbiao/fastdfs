a9534a3f024c76f1c534752816c2d8a6ab3c3e78
happyfish100 <happyfish100@0d4c4929-bc51-0410-a161-b7c81ae5c018>
2012-12-16 10:02:05 +0000

    add connection pool

--- common/connection_pool.c
+++ common/connection_pool.c
@@ -14,8 +14,16 @@
 #include "shared_func.h"
 #include "connection_pool.h"
 
-int conn_pool_init(ConnectionPool *cp, const int max_count_per_entry)
+int conn_pool_init(ConnectionPool *cp, int connect_timeout, \
+		const int max_count_per_entry)
 {
+	int result;
+
+	if ((result=init_pthread_lock(&cp->lock)) != 0)
+	{
+		return result;
+	}
+	cp->connect_timeout = connect_timeout;
 	cp->max_count_per_entry = max_count_per_entry;
 
 	return hash_init(&(cp->hash_array), simple_hash, 10, 0.75);
@@ -23,6 +31,11 @@ int conn_pool_init(ConnectionPool *cp, const int max_count_per_entry)
 
 void conn_pool_destroy(ConnectionPool *cp)
 {
+	pthread_mutex_lock(&cp->lock);
+	hash_destroy(&(cp->hash_array));
+	pthread_mutex_unlock(&cp->lock);
+
+	pthread_mutex_destroy(&cp->lock);
 }
 
 void conn_pool_disconnect_server(ConnectionInfo *pConnection)
@@ -73,6 +86,200 @@ int conn_pool_connect_server(ConnectionInfo *pConnection, \
 		pConnection->sock = -1;
 		return result;
 	}
+
+	return 0;
+}
+
+static int conn_pool_get_key(const ConnectionInfo *conn, char *key, int *key_len)
+{
+	struct in_addr sin_addr;
+
+        if (inet_aton(conn->ip_addr, &sin_addr) == 0)
+        {
+		*key_len = 0;
+                return EINVAL;
+        }
+
+	int2buff(sin_addr.s_addr, key);
+	*key_len = 4 + sprintf(key + 4, "%d", conn->port);
+
+	return 0;
+}
+
+ConnectionInfo *conn_pool_get_connection(ConnectionPool *cp, 
+	const ConnectionInfo *conn, int *err_no)
+{
+	char key[32];
+	int key_len;
+	int bytes;
+	char *p;
+	ConnectionManager *cm;
+	ConnectionNode *node;
+	ConnectionInfo *ci;
+
+	*err_no = conn_pool_get_key(conn, key, &key_len);
+	if (*err_no != 0)
+	{
+		return NULL;
+	}
+
+	pthread_mutex_lock(&cp->lock);
+	cm = (ConnectionManager *)hash_find(&cp->hash_array, key, key_len);
+	if (cm == NULL)
+	{
+		cm = (ConnectionManager *)malloc(sizeof(ConnectionManager));
+		if (cm == NULL)
+		{
+			*err_no = errno != 0 ? errno : ENOMEM;
+			logError("file: "__FILE__", line: %d, " \
+				"malloc %d bytes fail, errno: %d, " \
+				"error info: %s", __LINE__, \
+				(int)sizeof(ConnectionManager), \
+				*err_no, STRERROR(*err_no));
+			pthread_mutex_unlock(&cp->lock);
+			return NULL;
+		}
+
+		cm->head = NULL;
+		cm->total_count = 0;
+		cm->free_count = 0;
+		if ((*err_no=init_pthread_lock(&cm->lock)) != 0)
+		{
+			pthread_mutex_unlock(&cp->lock);
+			return NULL;
+		}
+		hash_insert(&cp->hash_array, key, key_len, cm);
+	}
+	pthread_mutex_unlock(&cp->lock);
+
+	pthread_mutex_lock(&cm->lock);
+	if (cm->head == NULL)
+	{
+		if ((cp->max_count_per_entry > 0) && 
+			(cm->total_count >= cp->max_count_per_entry))
+		{
+			*err_no = ENOSPC;
+			logError("file: "__FILE__", line: %d, " \
+				"connections: %d of server %s:%d " \
+				"exceed limit: %d", __LINE__, \
+				cm->total_count, conn->ip_addr, \
+				conn->port, cp->max_count_per_entry);
+			pthread_mutex_unlock(&cm->lock);
+			return NULL;
+		}
+
+		bytes = sizeof(ConnectionInfo) + sizeof(ConnectionNode);
+		p = (char *)malloc(bytes);
+		if (p == NULL)
+		{
+			*err_no = errno != 0 ? errno : ENOMEM;
+			logError("file: "__FILE__", line: %d, " \
+				"malloc %d bytes fail, errno: %d, " \
+				"error info: %s", __LINE__, \
+				bytes, *err_no, STRERROR(*err_no));
+			pthread_mutex_unlock(&cm->lock);
+			return NULL;
+		}
+
+		node = (ConnectionNode *)(p + sizeof(ConnectionInfo));
+		node->conn = (ConnectionInfo *)p;
+		node->manager = cm;
+		node->next = NULL;
+
+		cm->total_count++;
+		pthread_mutex_unlock(&cm->lock);
+	}
+	else
+	{
+		cm->free_count--;
+		ci = cm->head->conn;
+		cm->head = cm->head->next;
+		pthread_mutex_unlock(&cm->lock);
+
+		logDebug("file: "__FILE__", line: %d, " \
+			"server %s:%d, total_count: %d, free_count: %d", 
+			__LINE__, conn->ip_addr, conn->port, 
+			cm->total_count, cm->free_count);
+		return ci;
+	}
+
+	memcpy(node->conn, conn, sizeof(ConnectionInfo));
+	node->conn->sock = -1;
+	*err_no = conn_pool_connect_server(node->conn, cp->connect_timeout);
+	if (*err_no != 0)
+	{
+		free(p);
+		return NULL;
+	}
+
+	logDebug("file: "__FILE__", line: %d, " \
+		"server %s:%d, total_count: %d, free_count: %d", 
+		__LINE__, conn->ip_addr, conn->port, 
+		cm->total_count, cm->free_count);
+	return node->conn;
+}
+
+int conn_pool_close_connection_ex(ConnectionPool *cp, ConnectionInfo *conn, 
+	const bool bForce)
+{
+	char key[32];
+	int result;
+	int key_len;
+	ConnectionManager *cm;
+	ConnectionNode *node;
+
+	result = conn_pool_get_key(conn, key, &key_len);
+	if (result != 0)
+	{
+		return result;
+	}
+
+	pthread_mutex_lock(&cp->lock);
+	cm = (ConnectionManager *)hash_find(&cp->hash_array, key, key_len);
+	pthread_mutex_unlock(&cp->lock);
+	if (cm == NULL)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"hash entry of server %s:%d not exist", __LINE__, \
+			conn->ip_addr, conn->port);
+		return ENOENT;
+	}
+
+	node = (ConnectionNode *)(((char *)conn) + sizeof(ConnectionInfo));
+	if (node->manager != cm)
+	{
+		logError("file: "__FILE__", line: %d, " \
+			"manager of server entry %s:%d is invalid!", \
+			__LINE__, conn->ip_addr, conn->port);
+		return EINVAL;
+	}
+
+	pthread_mutex_lock(&cm->lock);
+	if (bForce)
+	{
+		cm->total_count--;
+
+		logDebug("file: "__FILE__", line: %d, " \
+			"server %s:%d, total_count: %d, free_count: %d", 
+			__LINE__, conn->ip_addr, conn->port, 
+			cm->total_count, cm->free_count);
+
+		conn_pool_disconnect_server(conn);
+		free(conn);
+	}
+	else
+	{
+		cm->free_count++;
+		node->next = cm->head;
+		cm->head = node;
+
+		logDebug("file: "__FILE__", line: %d, " \
+			"server %s:%d, total_count: %d, free_count: %d", 
+			__LINE__, conn->ip_addr, conn->port, 
+			cm->total_count, cm->free_count);
+			
+	}
+	pthread_mutex_unlock(&cm->lock);
 
 	return 0;
 }
